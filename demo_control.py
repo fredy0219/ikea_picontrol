b76f2c80d92e8bc5bea2d70aa64da82d26846695
fredy <fredy0219@gmail.com>
2017-11-05 00:40:09 +0800

    add rotary_encoder

--- demo_control.py
+++ demo_control.py
@@ -1,5 +1,12 @@
 import pigpio
 import time
+import rotary_encoder
+
+def callback(way):
+    global pos
+    pos += way
+    print("pos={}".format(pos))
+
 pi = pigpio.pi()
 
 pi.set_mode(25, pigpio.INPUT) #reed switch
@@ -7,6 +14,14 @@ pi.set_pull_up_down(25, pigpio.PUD_UP)
 pi.set_mode(24, pigpio.INPUT) #touch sensor
 pi.set_pull_up_down(24, pigpio.PUD_UP)
 
-while True:
-	print ("Reed switch at gpio25 : {0} , Touch sensor at gpio24 : {1}".format(pi.read(25),pi.read(24))
-	time.sleep(1)
+decoder = rotary_encoder.decoder(pi,5,6,callback)
+
+try:
+	while True:
+		print ("Reed switch at gpio25 : {0} , Touch sensor at gpio24 : {1}".format(pi.read(25),pi.read(24)))
+		time.sleep(1)
+
+except KeyboardInterrupt:
+	decoder.cancle()
+	pi.stop()
+
