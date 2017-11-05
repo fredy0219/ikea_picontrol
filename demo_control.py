2f799e3c180ef683b5b623ba486c4a2eff7a017c
fredy <fredy0219@gmail.com>
2017-11-05 09:12:52 +0800

    bug fixed

--- demo_control.py
+++ demo_control.py
@@ -13,6 +13,7 @@ def cbf_encoder(way):
 	oscmsg = OSC.OSCMessage()
 	oscmsg.setAddress("/enconder")
 	oscmsg.append(pos)
+	c.send(oscmsg)
 	print("pos={}".format(pos))
 
 def cbf_reed(gpio,level,tick):
@@ -52,10 +53,12 @@ pi = pigpio.pi()
 
 pi.set_mode(25, pigpio.INPUT) #reed switch
 pi.set_pull_up_down(25, pigpio.PUD_UP)
+pi.set_glitch_filter(25, 100)
 cb_reed = pi.callback(25,pigpio.EITHER_EDGE,cbf_reed)
 
 pi.set_mode(24, pigpio.INPUT) #touch sensor
 pi.set_pull_up_down(24, pigpio.PUD_UP)
+pi.set_glitch_filter(24, 100)
 cb_touch = pi.callback(24,pigpio.EITHER_EDGE,cbf_touch)
 
 decoder = rotary_encoder.decoder(pi,5,6,cbf_encoder)
